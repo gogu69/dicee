db4fbb5d655d9113b3e4dce33ebb30ae9f47e60b
Nicusor Iosef <nicu.iosef@gmail.com>
2025-10-14 16:32:04 +0300

    v1 css portret + functionalitati noi

--- src-test/testJs.js
+++ src-test/testJs.js
@@ -2,6 +2,7 @@
 
 let a;
 let b;
+const sub = document.querySelector(".sub");
 
 function arunca() {
     a = Math.trunc((Math.random()*6)+1);
@@ -10,17 +11,39 @@ function arunca() {
 }
 
 function mesaj() {
-
     let mesaj = "";
     if (a < b) {
         mesaj = "A castigat jucatorul B";
+        sub.classList.remove("zarA-color", "egalitate-color");
+        sub.classList.add("zarB-color");
+        
     }
     else if (a > b){
         mesaj = "A castigat jucatorul A";
+        sub.classList.remove("zarB-color","egalitate-color");
+        sub.classList.add("zarA-color");
     } 
     else {
         mesaj = "Egalitate!";
+        sub.classList.remove("zarA-color", "zarB-color");
+        sub.classList.add("egalitate-color");
     }
+
     return mesaj;
 }
+
+function animatieZaruri() {
+  const zarA = document.querySelector(".zarA");
+  const zarB = document.querySelector(".zarB");
+
+  zarA.classList.add("animate");
+  zarB.classList.add("animate");
+
+  // Elimină clasa după animație
+  setTimeout(() => {
+    zarA.classList.remove("animate");
+    zarB.classList.remove("animate");
+  }, 500);
+}
+
 // }
